0e00465c685a5866daf3c72993271d0141d7ac86
Marc Trojanowski <53346585+lux-g@users.noreply.github.com>
2021-07-19 22:33:13 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+import './styles/style.scss'
+//FONT AWESOME
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+//GSAP
+import { gsap, ScrollTrigger } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger); 
